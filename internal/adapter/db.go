300f72b5716be66d796615a9ef151af62cdd1e8e
Noah <noahskim7@gmail.com>
2026-03-26 17:33:24 -0700

    fix: postgres import to pgx

--- internal/adapter/db.go
+++ internal/adapter/db.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func New(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
-	db, err := sql.Open("postgres", addr)
+	db, err := sql.Open("pgx", addr)
 	if err != nil {
 		return nil, err
 	}
