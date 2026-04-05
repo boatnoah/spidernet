adac9cff8d929b3e9659ab566102447ea41a170f
Noah <noahskim7@gmail.com>
2026-04-05 16:50:09 -0700

    feat: added storage function to retrive all links by job id

--- internal/store/links.go
+++ internal/store/links.go
@@ -50,5 +50,34 @@ func (s *LinkStore) Create(ctx context.Context, linkPayload Links) error {
 	}
 
 	return nil
+}
 
+func (s *LinkStore) GetAllLinksByJobID(ctx context.Context, jobID uuid.UUID) (*[]Links, error) {
+	query := `
+		SELECT * FROM links
+		WHERE job_id = $1
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	var links []Links
+
+	rows, err := s.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var link Links
+
+		err = rows.Scan(&link.CrawlJobID, &link.FromURL, &link.ToURL, &link.Depth)
+		if err != nil {
+			return nil, err
+		}
+
+		links = append(links, link)
+	}
+
+	return &links, nil
 }
