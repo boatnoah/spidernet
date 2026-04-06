1cd840dd3152abe176e9c3053d27112dbf5f4bb6
Noah <noahskim7@gmail.com>
2026-04-06 09:19:10 -0700

    feat: added storage functions for pages

--- internal/store/pages.go
+++ internal/store/pages.go
@@ -1,10 +1,60 @@
 package store
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+// CREATE TABLE pages (
+// crawl_job_id UUID NOT NULL REFERENCES crawl_jobs(id) ON DELETE CASCADE,
+// url TEXT NOT NULL,
+// depth INT NOT NULL,
+// http_status INT,
+// fetch_error TEXT,
+// PRIMARY KEY (crawl_job_id, url)
+// );
+
+type Pages struct {
+	CrawlJobID uuid.UUID `json:"crawl_job_id"`
+	Url        string    `json:"url"`
+	Depth      string    `json:"depth"`
+	HttpStatus int       `json:"http_status"`
+	FetchError string    `json:"fetch_error"`
+}
 
 type PageStore struct {
 	db *sql.DB
 }
 
-type Pages struct {
+type PagePayload struct {
+	Url        string `json:"url"`
+	Depth      string `json:"depth"`
+	HttpStatus int    `json:"http_status"`
+	FetchError string `json:"fetch_error"`
+}
+
+func (s *PageStore) Create(ctx context.Context, p PagePayload) error {
+	query := `
+		INSERT INTO pages (url, depth, http_status, fetch_error)
+		VALUES ($1, $2, $3, $4)
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		p.Url,
+		p.Depth,
+		p.HttpStatus,
+		p.FetchError,
+	).Err()
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
