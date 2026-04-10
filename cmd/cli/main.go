b3729c321558daabd40eaafa804f0da6d70ea38f
Noah <noahskim7@gmail.com>
2026-04-09 17:14:25 -0700

    feat: added cli logic to make post requests to server

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+// spidernet <url> --depth=<int>
+
+func main() {
+	depth := flag.Int("depth", 1, "depth of the crawler")
+
+	flag.Parse()
+
+	if depth == nil {
+		fmt.Println("ex: spidernet <url> --depth=<int>")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	url := flag.Arg(0)
+
+	fmt.Println(url)
+	fmt.Println(*depth)
+
+}
